7d779dc9caed4055b94f0c4c557f2769b2fd7f62
lengtith <sotith1998@gmail.com>
2023-11-23 11:02:11 +0700

    update message data loading...

--- src/App.js
+++ src/App.js
@@ -16,10 +16,24 @@ const App = () => {
 
   // Get countries from api url
   const fetchData = async () => {
-    await fetch(`https://restcountries.com/v3.1/all`)
-      .then(response => response.json())
-      .then(data => setCountries(data))
-      .catch(err => console.log(err.message));
+    try {
+      const response = await fetch(`https://restcountries.com/v3.1/all`, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET',
+          'Access-Control-Allow-Headers': 'Origin, Content-Type, Accept',
+        },
+      });
+      
+      if (response.ok) {
+        const data = await response.json();
+        setCountries(data);
+      } else {
+        throw new Error(response.statusText);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
   };
 
   // Get specific country data for modal popup
